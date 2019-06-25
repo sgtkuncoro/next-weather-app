26a381f209f73175dd8c42311295de60ef512c35
Sigit Kuncoro <sigit.kunc@gmail.com>
2019-06-25 14:21:13 +0700

    Update dependencies

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const next = require("next");
 const routes = require("./routes");
-const app = next({ dev: process.env.NODE_ENV !== "production" });
+const app = next({ dev: process.env.APP_ENV !== "production" });
 const handler = routes.getRequestHandler(app);
 const address = require("address");
 
@@ -10,20 +10,21 @@ app.prepare().then(() => {
 
   server.use("/assets", express.static(`${__dirname}/static`));
 
-  // here you could do some express stuff if you fancy, eg. running custom middleware or offering an API
+  /**
+   * here you could do some express stuff if you fancy, eg. running custom middleware or offering an API
+   */
   // server.get('/api', (req, res) => {
   //   return res.send({ version: 1.0 })
   // });
 
-  // always make sure the handler is the last route entry
-
+  /** always make sure the handler is the last route entry  */
   server.use(handler);
 
-  const port = process.env.PORT || 3000;
+  const port = process.env.APP_PORT || 3000;
   server.listen(port, err => {
     if (err) throw err;
     console.log(
-      `> Env ${process.env.WILD_ENV} Ready on http://localhost:${port}`
+      `> Env ${process.env.APP_ENV} Ready on http://localhost:${port}`
     ); // eslint-disable-line no-console
 
     const url = `http://${address.ip()}:${port}`;
